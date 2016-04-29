90d5d52ac2e6314a461eb3031e4ab17236fdeb54
Akmat <pochta.akmat@gmail.com>
2016-04-29 14:52:37 +0600

    Скрипт подключения базы данных и вывод книг

--- programm/python/python.php
+++ programm/python/python.php
@@ -1,63 +1,54 @@
 <?
-echo "Это Python";
-?>
-					<table>
-                           <tr> 
-                                <td class="img"><img src="images/python.jpg"></td>
-                                <td class="p"><a href="#">В корзину</a><h3><a href="#">Al Vermeulen Scott W. Ambler - Elements of Java Style.</a></h3>
-                                <p>Ёжики защищены от внешней агрессии колючим 
-                                панцирем, который спасает их от тех, кто хочет поживиться вкусным и нежным мясом 
-                                ежа. Но не стоит считать это животное и совершенно безобидным, все-таки это 
-                                хищник.</p></td>
-                           </tr>
-                      </table>
-                   <table>
-                           <tr> 
-                                <td class="img"><img src="images/python.jpg"></td>
-                                <td class="p"><a href="#">В корзину</a><h3><a href="#">Al Vermeulen Scott W. Ambler - Elements of Java Style.</a></h3>
-                                <p>Ёжики защищены от внешней агрессии колючим 
-                                панцирем, который спасает их от тех, кто хочет поживиться вкусным и нежным мясом 
-                                ежа. Но не стоит считать это животное и совершенно безобидным, все-таки это 
-                                хищник.</p></td>
-                           </tr>
-                      </table>
-                   <table>
-                           <tr> 
-                                <td class="img"><img src="images/python.jpg"></td>
-                                <td class="p"><a href="#">В корзину</a><h3><a href="#">Al Vermeulen Scott W. Ambler - Elements of Java Style.</a></h3>
-                                <p>Ёжики защищены от внешней агрессии колючим 
-                                панцирем, который спасает их от тех, кто хочет поживиться вкусным и нежным мясом 
-                                ежа. Но не стоит считать это животное и совершенно безобидным, все-таки это 
-                                хищник.</p></td>
-                           </tr>
-                      </table>
-                   <table>
-                           <tr> 
-                                <td class="img"><img src="images/python.jpg"></td>
-                                <td class="p"><a href="#">В корзину</a><h3><a href="#">Al Vermeulen Scott W. Ambler - Elements of Java Style.</a></h3>
-                                <p>Ёжики защищены от внешней агрессии колючим 
-                                панцирем, который спасает их от тех, кто хочет поживиться вкусным и нежным мясом 
-                                ежа. Но не стоит считать это животное и совершенно безобидным, все-таки это 
-                                хищник.</p></td>
-                           </tr>
-                      </table>
-                   <table>
-                           <tr> 
-                                <td class="img"><img src="images/python.jpg"></td>
-                                <td class="p"><a href="#">В корзину</a><h3><a href="#">Al Vermeulen Scott W. Ambler - Elements of Java Style.</a></h3>
-                                <p>Ёжики защищены от внешней агрессии колючим 
-                                панцирем, который спасает их от тех, кто хочет поживиться вкусным и нежным мясом 
-                                ежа. Но не стоит считать это животное и совершенно безобидным, все-таки это 
-                                хищник.</p></td>
-                           </tr>
-                      </table>
-                   <table>
-                           <tr> 
-                                <td class="img"><img src="images/python.jpg"></td>
-                                <td class="p"><a href="#">В корзину</a><h3><a href="#">Al Vermeulen Scott W. Ambler - Elements of Java Style.</a></h3>
-                                <p>Ёжики защищены от внешней агрессии колючим 
-                                панцирем, который спасает их от тех, кто хочет поживиться вкусным и нежным мясом 
-                                ежа. Но не стоит считать это животное и совершенно безобидным, все-таки это 
-                                хищник.</p></td>
-                           </tr>
-                      </table>
+function db_connect(){
+        $host = 'localhost';
+        $user = 'Akmat';
+        $pswd = 'qwerty';
+        $db = 'diplom';
+        
+        $connection = mysql_connect($host, $user, $pswd);
+        if(!$connection || !mysql_select_db($db,$connection))
+        {
+            return false;
+        }
+        return $connection;
+    }
+
+    
+    
+    function db_result_to_array($result){
+        $res_array = array();
+        
+        $count = 0;
+        
+        while($row = mysql_fetch_array($result))
+        {
+            $res_array[$count] = $row;
+            $count++;
+        }
+        return $res_array;
+    }
+    
+    
+    function get_products(){
+        db_connect();
+        
+        $query = "SELECT * FROM kniga ORDER BY id DESC";
+        
+        $result = mysql_query($query);
+        
+        $result = db_result_to_array($result);
+        
+        return $result;
+    }
+	$products = get_products();
+	foreach($products as $item):?>
+		<table>
+				<tr>
+					<td class='img'><img src='images/python.jpg'></td>
+					<td class='p'><a href="<?=$item['id']?>">В корзину</a><h3><a href="<?=$item['id']?>"><?=$item['title']?></a></h3>
+					<p><?=$item['text']?></p></td>
+				</tr>
+		</table>
+<?
+	endforeach;
+?>	
