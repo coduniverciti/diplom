3c044f9772221d405a706a9a81e41f9953a0fc9b
Akmat <pochta.akmat@gmail.com>
2016-05-12 18:03:41 +0600

    Сделал баннер и его ставил

--- view/shablon/books.php
+++ view/shablon/books.php
@@ -36,73 +36,12 @@
           </div>
           <!--Header-->
               <!-- left menu -->
-              <!--div class="main">
-                <div  class="side">
-                    <ul class="menu">
-                        <li class="menu__list"><a href="#">Программирование</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=android">Android</a></li> 
-                                <li><a href="index.php?view=ci">С++</a></li> 
-                                <li><a href="index.php?view=delphi">Delphi</a></li> 
-                                <li><a href="index.php?view=java">Java</a></li> 
-                                <li><a href="index.php?view=python">Python</a></li> 
-                            </ul>
-                        </li>
-                        <li class="menu__list"><a href="#">Веб технологии</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=html">HTML</a></li> 
-                                <li><a href="index.php?view=javascript">JavaScript</a></li> 
-                                <li><a href="index.php?view=jquery">jQuery</a></li> 
-                                <li><a href="index.php?view=php">PHP</a></li> 
-                                <li><a href="index.php?view=perl">Perl</a></li> 
-                            </ul>
-                        </li> 
-                        <li class="menu__list"><a href="#">Безопасность</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=cript">Криптография</a></li> 
-                                <li><a href="index.php?view=security">Защита</a></li> 
-                                <li><a href="index.php?view=hack">Хакерство</a></li> 
-                                <li><a href="index.php?view=infec">Инфекции</a></li> 
-                            </ul>
-                        </li> 
-                        <li class="menu__list"><a href="#">Разработка</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=design">Управление</a></li> 
-                                <li><a href="index.php?view=menedj">Менеджмент</a></li> 
-                                <li><a href="index.php?view=project">Проектирование</a></li> 
-                                <li><a href="index.php?view=usability">Usability</a></li> 
-                            </ul>
-                        </li> 
-                        <li class="menu__list"><a href="#">Базы Данных</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=all">Общее</a></li> 
-                                <li><a href="index.php?view=oracle">Oracle</a></li> 
-                                <li><a href="index.php?view=mysql">MySQL</a></li> 
-                                <li><a href="index.php?view=mssql">MS SQL</a></li> 
-                                <li><a href="index.php?view=prochee">Прочее</a></li> 
-                            </ul>
-                        </li> 
-                        <li class="menu__list"><a href="#">ОС</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=unix">Unix</a></li> 
-                                <li><a href="index.php?view=linux">Linux</a></li> 
-                                <li><a href="index.php?view=freebsd">FreeBSD</a></li> 
-                                <li><a href="index.php?view=windows">Windows</a></li> 
-                                <li><a href="index.php?view=prochee_one">Прочее</a></li> 
-                            </ul>
-                        </li>
-                         <li class="menu__list"><a href="#">Теория</a>
-                            <ul class="menu__drop">
-                                <li><a href="index.php?view=all_one">Общее</a></li> 
-                                <li><a href="index.php?view=mat">Математика</a></li> 
-                                <li><a href="index.php?view=algoritm">Алгоритмы</a></li> 
-                                <li><a href="index.php?view=prochee_two">Прочее</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Гостевая книга</a></li>
-                    </ul>
-                </div>
-              </div-->
+              <div style="position: relative;width: 200px; height: 500px;float: left;margin: 30px 0 0 40px">
+                        <object width="200" height="500">
+                          <param name="movie" value="banner">
+                          <embed src="logo/banner.swf" width="200" height="500"></embed>
+                        </object>
+              </div>
               <!--Left menu-->  
 
                 <!--Контент-->    
